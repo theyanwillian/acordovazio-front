d48abca1922b37ae4666c893611e9f6b5025a1d7
theyanwillian <theyanwillian@gmail.com>
2022-10-08 17:30:57 -0300

    allow origin

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/a-cor-do-vazio'));
 
-app.get('/*', (req, res) =>{
-  res.sendFile(__dirname + '/dist/a-cor-do-vazio/index.html')
+app.get('/*', (req, res, next) => {
+    res.sendFile(__dirname + '/dist/a-cor-do-vazio/index.html')
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
 });
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function () {
     console.log('Listening on port %d', server_port);
 });
 

33006dce3af955c82eae3ae9ecd0b15e7f9f0e85
theyanwillian <theyanwillian@gmail.com>
2022-10-08 15:38:11 -0300

    testando aba players active route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from "./home/home.component";
+import { PlayerComponent } from './player/player.component';
 
 const routes: Routes = [
     // canActivate: [AuthGuard] canActivateChild: [AuthGuard]
     // children: [{path: '', component: ''}]
     {path: '', component: HomeComponent},
     // {path: 'ranking', component: RankingComponent},
-    // {path: 'players', component: PlayerComponent},
+    {path: 'players', component: PlayerComponent},
     // {path: 'player/create', component: PlayerDialogModalComponent, outlet: 'popup'},
     // {path: 'player/update/:id', component: PlayerDialogModalComponent, outlet: 'popup'},
     // {path: 'player/detail/:id', component: PlayerDetailModalComponent, outlet: 'popup'},

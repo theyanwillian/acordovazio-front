48d5b07cacd063008f94925c9581253d7bbac2b7
theyanwillian <theyanwillian@gmail.com>
2022-10-08 18:08:38 -0300

    v1 funcional alterar https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://a-cor-do-vazio-rpg-backend.herokuapp.com/'
+    baseUrl: 'https://a-cor-do-vazio-rpg-backend.herokuapp.com/'
 };
